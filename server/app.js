d10771ab19ecf2a49ab6cd31dd15ffa7f463ce41
J19MJ04 <s.traore-ext@malakoffmederic-humanis.com>
2020-01-09 17:19:36 +0100

    mean stack initialized

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = app; 
